edb5607da686f7130d252913986fa298c1f369c0
darshana weerasooriya <96476541+darshanaweerasooriya@users.noreply.github.com>
2025-10-02 18:54:25 +0530

    Project strucutre done

---
+++ EmployeeManagement/Controllers/CalculatorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeManagement.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        // GET: Calculator
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
