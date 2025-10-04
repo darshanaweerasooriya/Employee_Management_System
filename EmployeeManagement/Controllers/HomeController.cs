e4c0a7702dac98f7cf8edf33a59de7036b856044
darshana weerasooriya <96476541+darshanaweerasooriya@users.noreply.github.com>
2025-10-04 08:43:48 +0530

    Frontend Structure was created

--- EmployeeManagement/Controllers/HomeController.cs
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -8,23 +8,38 @@ namespace EmployeeManagement.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
+        public ActionResult Index() => View();
 
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
             return View();
+        }
+
+        // Login action
+        public ActionResult Login()
+        {
+            // Specify the path to the view inside Views/Account
+            return View("~/Views/Account/Login.cshtml");
+        }
+
+        [HttpPost]
+        public ActionResult Login(string Email, string Password)
+        {
+            if (Email == "admin@example.com" && Password == "123")
+            {
+                Session["User"] = Email;
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Message = "Invalid email or password";
+            return View("~/Views/Account/Login.cshtml");
         }
     }
 }
